27ccefbbdd231b76eede6c1b904fccadaf1b4c77
BharathV16 <156333596+BharathV16@users.noreply.github.com>
2024-01-23 21:15:14 +0530

    src

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import BMI from './component1/bmi';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>BMI CALCULATOR</h1>
+    <BMI/>
+        </div>
+  );
+}
+
+export default App;
